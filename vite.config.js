b47e6b1c0335ca4f3465b9dab52673fd669a531f
Fiona Waters <fiwaters6@gmail.com>
2023-03-24 23:22:45 +0000

    working on pwa functionality

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,48 @@ export default defineConfig({
     svgr(),
     react(),
     VitePWA({
+      includeAssets: ['logo.svg'],
+      manifest: {
+          "theme_color": "#ffffff",
+          "background_color": "#ffffff",
+          "display": "standalone",
+          "scope": "/",
+          "start_url": "/",
+          "name": "Yarn Swap",
+          "short_name": "Yarn Swap",
+          "description": "Yarn Swap Progressive Web Application",
+          "icons": [
+              {
+                  "src": "/icon-192x192.png",
+                  "sizes": "192x192",
+                  "type": "image/png"
+              },
+              {
+                  "src": "/icon-256x256.png",
+                  "sizes": "256x256",
+                  "type": "image/png"
+              },
+              {
+                  "src": "/icon-384x384.png",
+                  "sizes": "384x384",
+                  "type": "image/png"
+              },
+              {
+                  "src": "/icon-512x512.png",
+                  "sizes": "512x512",
+                  "type": "image/png"
+              }
+          ]
+      },
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true
       },
-      injectRegister: 'auto'
+      injectRegister: 'auto',
+      workbox: {
+        clientsClaim: true,
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      }
     })
   ],
   server: {
