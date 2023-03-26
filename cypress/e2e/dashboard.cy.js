786159da23843b6f2660113f11e5136c788273ef
Fiona Waters <fiwaters6@gmail.com>
2023-03-26 21:44:18 +0100

    Updated Test

--- cypress/e2e/dashboard.cy.js
+++ cypress/e2e/dashboard.cy.js
@@ -1,5 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:4173/dashboard')
+    cy.get("a [href='/dashboard]").children().contains("Dashboard")
   })
 })
